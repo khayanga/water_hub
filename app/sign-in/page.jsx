4edbdb756e31d060a309cb250dfba1fff8c623ba
khayanga <khayangavirgil@gmail.com>
2024-10-31 10:05:56 +0300

    sign in

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -41,18 +41,19 @@ const SignIn = () => {
       });
   
       const result = await response.json();
+      console.log("Response OK:", response.ok, "Result:", result);
   
       if (response.ok && result.access_token) {
         console.log(`Access Token: ${result.access_token}`);
         localStorage.setItem('accessToken', result.access_token); 
-        
-        
         sessionStorage.setItem('user', JSON.stringify(result.data));
   
-        login(result.data); 
+        login(result.data);
         setEmail('');
         setPassword('');
-        router.push('/client-dashboard'); 
+        
+        // alert("Redirecting to dashboard");
+        router.push('/client-dashboard');
       } else {
         setError(result.message || 'Failed to sign in. Please check your email and password.');
       }
